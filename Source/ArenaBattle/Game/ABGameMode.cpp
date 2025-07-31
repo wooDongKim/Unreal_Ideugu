f0ac12897e24d291041a1a32bc5004a8d8ea1b85
simg\SSimg <dfmf555@simglab.com>
2025-07-31 12:52:38 +0900

    복제

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "ABGameMode.h"
+
+
+AABGameMode::AABGameMode()
+{
+	//static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	//if (ThirdPersonClassRef.Class)
+	//{
+	//	DefaultPawnClass = ThirdPersonClassRef.Class;
+	//}
+	// DefaultPawnClass
+
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/ArenaBattle.ABCharacterPlayer"));
+	if (DefaultPawnClassRef.Class)
+	{
+		DefaultPawnClass = DefaultPawnClassRef.Class;
+	}
+
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerRef(TEXT("/Script/ArenaBattle.ABPlayerController"));
+	if (PlayerControllerRef.Class)
+	{
+		PlayerControllerClass = PlayerControllerRef.Class;
+	}
+}
